74897d9b21c0feaa95d73730325599dd8759b003
2mahm <mahrew2019@gmail.com>
2026-02-17 21:27:08 +0200

    remover status

--- route_api/views.py
+++ route_api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.response import Response, status
+from rest_framework.response import Response
 from rest_framework import status
 from django.core.cache import cache
 
